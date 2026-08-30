c4b63a328cf6ef510fe051fd3acd0850ee903b55
Paulo-SergioAlves <jxnyyn@gmail.com>
2026-08-30 13:10:47 -0300

    Initial commit

---
+++ src/test/PlantioMudasTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import impacta.PlantioMudas;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlantioMudasTest {
+
+    @Test
+    @DisplayName("Deve Calcular corretamente a Pontuação do Plantio de mudas")
+    public void deveCalcularPontuacaoPlantio() {
+        PlantioMudas plantio = new PlantioMudas(
+                1,"Plantio de arvores","Plantio de mudas",LocalDateTime.now(),20,10
+                );
+        assertEquals(25,plantio.calcularPontuacao());
+
+
+    }
+
+}
