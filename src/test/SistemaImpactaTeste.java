0740e56d5b3b0f592367d7374eb5232d79624e47
Paulo-SergioAlves <jxnyyn@gmail.com>
2026-09-02 17:22:29 -0300

    adicionei o @teste deveOrdenarVoluntariosPorPontuacao e o @test deveDesempatarPorNome

--- src/test/SistemaImpactaTeste.java
+++ src/test/SistemaImpactaTeste.java
@@ -1,5 +1,6 @@
 package test;
-
+import impacta.Ranking;
+import impacta.Voluntario;
 import impacta.PlantioMudas;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,4 +22,89 @@ public class SistemaImpactaTeste {
 
     }
 
+    @Test
+    void deveOrdenarVoluntariosPorPontuacao() {
+
+        Voluntario voluntario1 =
+                new Voluntario("Daniel", "daniel@email.com", "001");
+
+        Voluntario voluntario2 =
+                new Voluntario("Lucas", "lucas@email.com", "002");
+
+        Voluntario voluntario3 =
+                new Voluntario("Paulo", "paulo@email.com", "003");
+
+        voluntario1.adicionarParticipacao(20);
+        voluntario2.adicionarParticipacao(50);
+        voluntario3.adicionarParticipacao(30);
+
+        java.util.List<Voluntario> voluntarios =
+                new java.util.ArrayList<>();
+
+        voluntarios.add(voluntario1);
+        voluntarios.add(voluntario2);
+        voluntarios.add(voluntario3);
+
+        String[] resultado =
+                Ranking.ordenarVoluntarios(voluntarios);
+
+        assertEquals(
+                "Lucas - 1 ações - 50 pontos",
+                resultado[0]
+        );
+
+        assertEquals(
+                "Paulo - 1 ações - 30 pontos",
+                resultado[1]
+        );
+
+        assertEquals(
+                "Daniel - 1 ações - 20 pontos",
+                resultado[2]
+        );
+    }
+
+    @Test
+    void deveDesempatarPorNome() {
+
+        Voluntario voluntario1 =
+                new Voluntario("Daniel", "daniel2@email.com", "004");
+
+        Voluntario voluntario2 =
+                new Voluntario("Lucas", "lucas2@email.com", "005");
+
+        Voluntario voluntario3 =
+                new Voluntario("Paulo", "paulo2@email.com", "006");
+
+        voluntario1.adicionarParticipacao(60);
+        voluntario2.adicionarParticipacao(60);
+        voluntario3.adicionarParticipacao(10);
+
+        java.util.List<Voluntario> voluntarios =
+                new java.util.ArrayList<>();
+
+        voluntarios.add(voluntario1);
+        voluntarios.add(voluntario2);
+        voluntarios.add(voluntario3);
+
+        String[] resultado =
+                Ranking.ordenarVoluntarios(voluntarios);
+
+        assertEquals(
+                "Daniel - 1 ações - 60 pontos",
+                resultado[0]
+        );
+
+        assertEquals(
+                "Lucas - 1 ações - 60 pontos",
+                resultado[1]
+        );
+
+        assertEquals(
+                "Paulo - 1 ações - 10 pontos",
+                resultado[2]
+        );
+    }
 }
+
+
