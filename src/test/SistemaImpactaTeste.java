2dc364d92e3d2ecb23999bc79e01b77c67ee2fb8
Paulo-SergioAlves <jxnyyn@gmail.com>
2026-09-02 21:23:32 -0300

    Adicionei o método de teste deveOrdenarVoluntariosPorPontuacao

--- src/test/SistemaImpactaTeste.java
+++ src/test/SistemaImpactaTeste.java
@@ -1,4 +1,56 @@
 package test;
 
+import impacta.Ranking;
+import impacta.Voluntario;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SistemaImpactaTeste {
+
+    @Test
+    @DisplayName(" Deve ordenar os voluntários por pontuação ")
+    public void deveOrdenarVoluntariosPorPontuacao() {
+
+        Voluntario voluntario1 =
+                new Voluntario("Daniel", "daniel@email.com", "001");
+
+        Voluntario voluntario2 =
+                new Voluntario("Lucas", "lucas@email.com", "002");
+
+        Voluntario voluntario3 =
+                new Voluntario("Paulo", "paulo@email.com", "003");
+
+        voluntario1.adicionarParticipacao(20);
+        voluntario2.adicionarParticipacao(50);
+        voluntario3.adicionarParticipacao(30);
+
+        java.util.List<Voluntario> voluntarios =
+                new java.util.ArrayList<>();
+
+        voluntarios.add(voluntario1);
+        voluntarios.add(voluntario2);
+        voluntarios.add(voluntario3);
+
+        String[] resultado =
+                Ranking.ordenarVoluntarios(voluntarios);
+
+        assertEquals(
+                "Lucas - 1 ações - 50 pontos",
+                resultado[0]
+        );
+
+        assertEquals(
+                "Paulo - 1 ações - 30 pontos",
+                resultado[1]
+        );
+
+        assertEquals(
+                "Daniel - 1 ações - 20 pontos",
+                resultado[2]
+        );
+    }
+
+
+
 }
